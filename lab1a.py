f7362ce723d3904d4bb0c3e712bc630906f7bae6
MaximillianHavrylyuk <havrmax@yahoo.ca>
2026-01-15 13:54:43 -0500

    lab1a.py
    Added comments to explain variable creation and type checking.

--- lab1a.py
+++ lab1a.py
@@ -1,1 +1,24 @@
+#Author:Maximillian Havrylyuk
+#Lab1a.py
+#making variables and using the type() function along with the print() function.
 
+#Created a variable with the message Welcome to PRG101.
+message = "Welcome to PRG101"
+
+#printing the variable I just created.
+print(message)
+
+#checking what data type the variable "message" has.
+type(message)
+
+#concantenating my variable and adding the string " Python is the future."
+print(message + " Python is the future.")
+
+#making a new variable called "x" and checking the data type of the variable.
+x = 10
+type(x)
+
+#reassigning the variable "x" and checking and printing its data type.
+x = "hello"
+print(type(x))
+#I observed that the variable x changed from a integer to a string when reassigning the value.
